c91a008e895b50a8b3a5875d28c11656b13d1fb0
Afaq <afaqali4244@gmail.com>
2026-02-12 08:23:03 +0500

    Done with creat task feature

--- src/components/modals/create-task/index.tsx
+++ src/components/modals/create-task/index.tsx
@@ -50,7 +50,7 @@ const CreateTaskModal = () => {
     console.log("Submit triggered");
     try {
       if (workspaceId && projectId) {
-        const { attachments, assignees, linkName, ...apiData } = data; // Remove form-only fields
+        const { attachments, linkName, ...apiData } = data; // Remove form-only fields
 
         showSubmissionToast(apiData);
         await createTask.mutateAsync({
